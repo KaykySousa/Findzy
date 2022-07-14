a8018c454d4e55722ac8f1cc356c07ef498db488
KaykySousa <kaykysousa321@gmail.com>
2022-07-14 15:11:39 -0300

    initial project

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div className="flex min-h-screen w-full flex-col items-center justify-center">
+			<h1 className="text-4xl font-bold">FINDZY</h1>
+			<p className="text-xl">Sua plataforma web de Achados e Perdidos</p>
+		</div>
+	)
+}
