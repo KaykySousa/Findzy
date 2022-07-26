4553c9bed3a3c7faf12922f179a4083f97ef2e7c
KaykySousa <kaykysousa321@gmail.com>
2022-07-25 22:25:16 -0300

    update: tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
 	content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+			},
+			backgroundImage: {
+				"box-login-texture": "url('/box.svg')",
+			},
+		},
 	},
 	plugins: [
 		require("@tailwindcss/forms"),
