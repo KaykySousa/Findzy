49a83024dcbccda18335c08fcc7f0080e01a2648
KaykySousa <kaykysousa321@gmail.com>
2022-07-25 22:30:54 -0300

    add: notifications library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { ToastContainer } from "react-toastify"
+import "../styles/globals.css"
+
+import "react-toastify/dist/ReactToastify.min.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				draggableDirection="x"
+				draggablePercent={60}
+				pauseOnHover
+				theme="colored"
+			/>
+		</>
+	)
 }
 
 export default MyApp
