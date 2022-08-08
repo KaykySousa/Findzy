e731506ab1538fff48160b617605d812662ed21d
KaykySousa <kaykysousa321@gmail.com>
2022-08-07 22:41:13 -0300

    add: 404 custom page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { LinkButton } from "@/design/index"
+
+export default function Error404() {
+	return (
+		<div className="flex min-h-screen w-full flex-col items-center p-10">
+			<span className="text-xl text-purple-700">
+				<strong>Findzy</strong> Perdeu? Achou!
+			</span>
+			<div className="flex w-full max-w-lg flex-1 flex-col items-center justify-center gap-y-6">
+				<h1 className="text-6xl font-bold text-purple-700">404</h1>
+				<p className="text-center text-3xl">
+					<strong>Poxa...</strong> Aqui não tem nada!
+				</p>
+				<LinkButton href="/">Voltar ao início</LinkButton>
+			</div>
+		</div>
+	)
+}
