699b32a777669bb690b8c36e1b5c33aed314ca83
KaykySousa <kaykysousa321@gmail.com>
2022-08-07 22:39:44 -0300

    update: style

--- src/components/design/Button.tsx
+++ src/components/design/Button.tsx
@@ -2,18 +2,19 @@ import { ButtonHTMLAttributes } from "react"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	theme?: "primary" | "secondary"
+	loading?: boolean
 }
 
 export default function Button({
 	className,
 	theme = "primary",
+	loading,
 	...props
 }: ButtonProps) {
 	const themes = {
 		primary:
 			"h-12 w-full rounded-md bg-purple-700 text-lg font-bold text-white",
-		secondary:
-			"h-12 w-full rounded-md bg-white font-bold text-purple-700 border-2 border-purple-700",
+		secondary: "h-12 w-full rounded-md bg-white font-bold text-purple-700",
 	}
 
 	return <button className={themes[theme] + ` ${className}`} {...props} />
