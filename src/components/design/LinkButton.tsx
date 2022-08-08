f06f0fd8b5116cf0ee6f36c8c62858d8012f0e5f
KaykySousa <kaykysousa321@gmail.com>
2022-08-07 22:40:53 -0300

    add: button classname

--- src/components/design/LinkButton.tsx
+++ src/components/design/LinkButton.tsx
@@ -6,18 +6,20 @@ interface LinkButtonProps
 	extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, keyof LinkProps>,
 		LinkProps {
 	theme?: "primary" | "secondary"
+	buttonClassName?: string
 }
 
 export default function LinkButton({
 	href,
 	theme = "primary",
 	className,
+	buttonClassName,
 	...props
 }: LinkButtonProps) {
 	return (
 		<Link href={href}>
 			<a className={`w-full ${className}`}>
-				<Button theme={theme} {...props} />
+				<Button theme={theme} className={buttonClassName} {...props} />
 			</a>
 		</Link>
 	)
