839c4e9b01e1ac1bbf83f4e6bd61c9c3edf2b632
KaykySousa <kaykysousa321@gmail.com>
2022-10-16 12:26:41 -0300

    add: profile picture upload and address on company register

--- src/pages/api/register/company.ts
+++ src/pages/api/register/company.ts
@@ -5,7 +5,8 @@ import CustomError from "@/utils/CustomError"
 import handleError from "@/utils/handleError"
 import tokenProvider from "@/utils/tokenProvider"
 
-import type { NextApiHandler } from "next"
+import imageUploader from "@/utils/imageUploader"
+import type { NextApiHandler, PageConfig } from "next"
 
 const handler: NextApiHandler = async (req, res) => {
 	try {
@@ -15,26 +16,58 @@ const handler: NextApiHandler = async (req, res) => {
 			})
 		}
 
-		const { name, cnpj, cep, number, email, phone, password, address } = req.body
+		const {
+			name,
+			cnpj,
+			cep,
+			number,
+			email,
+			phone,
+			password,
+			address,
+			profilePicture,
+		} = req.body
 
-		if (!name || !cnpj || !cep || !number || !email || !phone || !password || !address)
+		if (
+			!name ||
+			!cnpj ||
+			!cep ||
+			!number ||
+			!email ||
+			!phone ||
+			!password ||
+			!address ||
+			!profilePicture
+		)
 			throw new CustomError("Insira todos os dados")
 
 		if (password.length < 8) throw new CustomError("Senha fraca")
+
+		const { secure_url: profilePictureUrl } = await imageUploader(
+			profilePicture
+		)
 
 		const passwordHash = await hash(password, 10)
 
 		const company = await prisma.company.create({
 			data: {
-				address,
-				cep,
 				cnpj,
 				email,
 				name,
 				number,
 				password: passwordHash,
-				phone
-			}
+				phone,
+				profile_picture_url: profilePictureUrl,
+				address: {
+					create: {
+						cep,
+						city: address.city,
+						district: address.district,
+						street: address.street,
+						uf: address.uf,
+					},
+				},
+			},
 		})
 
 		const token = tokenProvider(company.id)
@@ -44,12 +77,21 @@ const handler: NextApiHandler = async (req, res) => {
 			company: {
 				name: company.name,
 				email: company.email,
-			}
+			},
 		})
 	} catch (e) {
+		console.log(e)
 		const { error, statusCode } = handleError(e)
 		return res.status(statusCode).json({ error })
 	}
+}
+
+export const config: PageConfig = {
+	api: {
+		bodyParser: {
+			sizeLimit: "4mb",
+		},
+	},
 }
 
 export default handler
