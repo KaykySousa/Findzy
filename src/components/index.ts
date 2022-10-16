1dcb5b29dbf36daa1f759bacc9ce903a3355a8e5
KaykySousa <kaykysousa321@gmail.com>
2022-10-16 12:10:13 -0300

    add: file to import components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import CompanyBanner from "@/components/CompanyBanner"
+import CompanyCard from "@/components/CompanyCard"
+import Header from "@/components/Header"
+import ImageUpload from "@/components/ImageUpload"
+import ItemCard from "@/components/ItemCard"
+import Logo from "@/components/Logo"
+
+export { CompanyCard, CompanyBanner, ItemCard, Header, ImageUpload, Logo }
