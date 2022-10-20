da31c2480823160e111109069097a5aba3125373
KaykySousa <kaykysousa321@gmail.com>
2022-10-19 21:33:46 -0300

    add: input and select components to index

--- src/components/design/index.ts
+++ src/components/design/index.ts
@@ -1,6 +1,8 @@
 import Anchor from "./Anchor"
 import Button from "./Button"
 import FloatingInput from "./FloatingInput"
+import Input from "./Input"
 import LinkButton from "./LinkButton"
+import Select from "./Select"
 
-export { Anchor, Button, FloatingInput, LinkButton }
+export { Anchor, Button, FloatingInput, Input, Select, LinkButton }
