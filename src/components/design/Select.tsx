b7699e754772b326e64d5f07feca2028debacff7
KaykySousa <kaykysousa321@gmail.com>
2022-10-19 21:35:33 -0300

    add: select component

---
+++ src/components/design/Select.tsx
@@ -0,0 +1,65 @@
+import { SelectHTMLAttributes, useState } from "react"
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+	textLabel?: string
+	options: string[]
+	inputClassName?: string
+	defaultOption?: string
+}
+
+export default function Select({
+	textLabel,
+	options,
+	className,
+	inputClassName,
+	defaultOption,
+	onChange,
+	value,
+	...props
+}: SelectProps) {
+	const [isDefault, setIsDefault] = useState(true)
+
+	return (
+		<div
+			className={`relative flex w-full flex-col justify-center ${className}`}
+		>
+			{textLabel && (
+				<label className="mb-1 w-full font-bold text-gray-800">
+					{textLabel}
+				</label>
+			)}
+			<select
+				className={`h-10 w-full cursor-text rounded border-0 bg-gray-100 focus:border-purple-700 focus:ring-0 ${inputClassName} ${
+					isDefault ? "text-sm text-gray-500" : ""
+				}`}
+				onChange={(e) => {
+					if (e.target.value === "defaultOption") {
+						setIsDefault(true)
+					} else {
+						setIsDefault(false)
+					}
+					if (onChange) {
+						onChange(e)
+					}
+				}}
+				value={value || "defaultOption"}
+				{...props}
+			>
+				{defaultOption && (
+					<option value={"defaultOption"} hidden disabled>
+						{defaultOption}
+					</option>
+				)}
+				{options.map((optionValue, index) => (
+					<option
+						className="text-base text-black"
+						value={optionValue.toLowerCase()}
+						key={index}
+					>
+						{optionValue}
+					</option>
+				))}
+			</select>
+		</div>
+	)
+}
