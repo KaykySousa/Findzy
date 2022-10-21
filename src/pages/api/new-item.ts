3f2e3cb87078295d6c7afe691a595f61f2addb6f
KaykySousa <kaykysousa321@gmail.com>
2022-10-20 22:21:33 -0300

    add: promise to upload the images and after save the url in database

--- src/pages/api/new-item.ts
+++ src/pages/api/new-item.ts
@@ -4,31 +4,50 @@ import handleError from "@/utils/handleError"
 import imageUploader from "@/utils/imageUploader"
 import { NextApiHandler } from "next"
 
-interface imagesUrlData {
+interface ImagesUrlData {
 	image_url: string
+}
+
+interface BodyData {
+	category: string
+	color: string
+	description: string
+	local: string
+	name: string
+	images: string[]
 }
 
 const handler: NextApiHandler = async (req, res) => {
 	try {
-		console.log("FUNCIONA")
 		if (req.method !== "POST") {
 			throw new CustomError("Method not allowed", {
 				statusCode: 405,
 			})
 		}
 
-		let imagesUrl: imagesUrlData[] = []
+		let imagesUrl: ImagesUrlData[] = []
 
-		const { category, color, description, local, name, images } = req.body
+		const { category, color, description, local, name, images }: BodyData =
+			req.body
 
 		if (!category || !color || !description || !local || !name || !images) {
 			throw new CustomError("Missing parameters")
 		}
 
-		;(images as string[]).map(async (image) => {
-			const { secure_url: imageUrl } = await imageUploader(image)
-			imagesUrl.push({ image_url: imageUrl })
+		await new Promise((resolve, reject) => {
+			images.map(async (image) => {
+				const { secure_url: imageUrl } = await imageUploader(
+					image,
+					"findzy-items"
+				)
+				imagesUrl.push({ image_url: imageUrl })
+				if (imagesUrl.length === images.length) {
+					resolve(imagesUrl)
+				}
+			})
 		})
+
+		console.log(imagesUrl)
 
 		const item = await prisma.item.create({
 			data: {
@@ -38,11 +57,7 @@ const handler: NextApiHandler = async (req, res) => {
 				name,
 				image: {
 					createMany: {
-						data: [
-							{
-								image_url: "lfasjhfkashfkashfaksf",
-							},
-						],
+						data: imagesUrl,
 					},
 				},
 			},
