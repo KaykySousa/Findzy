e57981c82f1ddc41bcf0d346091bf78d78c3966f
KaykySousa <kaykysousa321@gmail.com>
2022-10-23 22:48:00 -0300

    add: render the items in item card components

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -1,11 +1,25 @@
 import { LinkButton } from "@/components/design"
 import { CompanyBanner, Header, ItemCard } from "@/components/index"
 import getCompany from "@/utils/getCompany"
+import getItems from "@/utils/getItems"
 import { PlusIcon } from "@heroicons/react/24/outline"
 import { GetServerSideProps } from "next"
 import { parseCookies } from "nookies"
 
-export default function MainCompany() {
+interface MainCompanyProps {
+	items:
+		| {
+				images: string[]
+				id: string
+				name: string
+				color: string
+				local: string
+				description: string
+		  }[]
+		| undefined
+}
+
+export default function MainCompany({ items }: MainCompanyProps) {
 	return (
 		<div className="flex min-h-screen w-full flex-col items-center bg-white">
 			<Header />
@@ -20,10 +34,16 @@ export default function MainCompany() {
 						<PlusIcon className="mr-1 h-6 w-6 md:mb-4 md:h-10 md:w-10" />
 						<span>NOVO ITEM</span>
 					</LinkButton>
-					<ItemCard />
-					<ItemCard />
-					<ItemCard />
-					<ItemCard />
+					{items?.map((item, index) => (
+						<ItemCard
+							key={index}
+							color={item.color}
+							description={item.description}
+							local={item.local}
+							title={item.name}
+							images={item.images}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
@@ -43,11 +63,16 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			},
 		}
 
+	const items = await getItems()
+
+	console.log(items)
+
 	return {
 		props: {
 			company: {
 				name: companyData.name,
 			},
+			items,
 		},
 	}
 }
