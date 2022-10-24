77ab9c3bc02ff102184dbbb018a4926561a8873b
KaykySousa <kaykysousa321@gmail.com>
2022-10-23 22:45:32 -0300

    add: show input prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,27 @@ import {
 	MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline"
 
-export default function Header() {
+interface HeaderProps {
+	showInput?: boolean
+	className?: string
+}
+
+export default function Header({ showInput = true, className }: HeaderProps) {
 	return (
-		<header className="flex h-14 w-full items-center gap-x-4 border-b border-gray-100 bg-white px-2 md:h-16 md:justify-between md:px-8">
+		<header
+			className={`flex h-14 w-full items-center gap-x-4 border-b border-gray-100 bg-white px-2 md:h-16 md:justify-between md:px-8 ${className}`}
+		>
 			<Logo className="hidden md:block" />
-			<Input
-				icon={
-					<MagnifyingGlassIcon className="absolute left-2 h-5 w-5 text-purple-700" />
-				}
-				placeholder="Buscar por item ou estabelecimento"
-				className="md:max-w-3xl"
-				inputClassName="!h-8"
-			/>
+			{showInput && (
+				<Input
+					icon={
+						<MagnifyingGlassIcon className="absolute left-2 h-5 w-5 text-purple-700" />
+					}
+					placeholder="Buscar por item ou estabelecimento"
+					className="md:max-w-3xl"
+					inputClassName="!h-8"
+				/>
+			)}
 			<div className="flex gap-x-4">
 				<ChatBubbleBottomCenterTextIcon className="h-6 w-6 text-purple-700" />
 				<Cog8ToothIcon className="h-6 w-6 text-purple-700" />
