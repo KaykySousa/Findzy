724ee865e43c87421de302691fbacd29aa5f9fbf
KaykySousa <kaykysousa321@gmail.com>
2022-10-30 20:13:25 -0300

    add: link to the home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link"
 import { HTMLAttributes } from "react"
 
 interface LogoProps extends HTMLAttributes<HTMLParagraphElement> {}
 
 export default function Logo({ className, ...props }: LogoProps) {
 	return (
-		<p
-			className={`whitespace-nowrap text-lg text-purple-700 ${className}`}
-			{...props}
-		>
-			<strong>Findzy</strong> Perdeu? Achou!
-		</p>
+		<Link href="/">
+			<a className={`text-lg text-purple-700 ${className}`}>
+				<p className="whitespace-nowrap" {...props}>
+					<strong>Findzy</strong> Perdeu? Achou!
+				</p>
+			</a>
+		</Link>
 	)
 }
