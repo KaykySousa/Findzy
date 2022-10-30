c6a64838cc6e78e18f541c357b1699601b37a76c
KaykySousa <kaykysousa321@gmail.com>
2022-10-30 20:34:23 -0300

    update: get items from a company by id not all items

--- src/utils/getItems.ts
+++ src/utils/getItems.ts
@@ -1,10 +1,18 @@
 import prisma from "@/prisma/client"
 import CustomError from "./CustomError"
 
-export default async function getItems() {
+export default async function getItems(companyId: string) {
 	try {
 		const items = await prisma.item.findMany({
-			include: {
+			where: {
+				company_id: companyId,
+			},
+			select: {
+				color: true,
+				description: true,
+				id: true,
+				local: true,
+				name: true,
 				images: {
 					select: {
 						image_url: true,
