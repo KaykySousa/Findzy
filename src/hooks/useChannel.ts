8570cdf4ec5f96c670cf986f403a6dd978376ef9
KaykySousa <kaykysousa321@gmail.com>
2022-11-02 14:44:50 -0300

    add: use channel hook to subscribe in a channel

---
+++ src/hooks/useChannel.ts
@@ -0,0 +1,29 @@
+import { Types } from "ably"
+import { Realtime } from "ably/promises"
+import { useEffect } from "react"
+
+const ably = new Realtime.Promise({
+	authUrl: `${
+		process.env.VERCEL_URL || "http://localhost:3000"
+	}/api/ably-token`,
+})
+
+export default function useChannel(
+	channelName: string,
+	callbackOnMessage: (message: Types.Message) => void
+) {
+	const channel = channelName ? ably.channels.get(channelName) : null
+
+	useEffect(() => {
+		if (!channel) return
+
+		channel.subscribe((message) => {
+			callbackOnMessage(message)
+		})
+		return () => {
+			channel.unsubscribe()
+		}
+	})
+
+	return channel
+}
