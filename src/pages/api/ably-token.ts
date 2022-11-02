bb8920e65a7fd561ae33904c7a61ef37b2a8cd51
KaykySousa <kaykysousa321@gmail.com>
2022-11-02 14:48:04 -0300

    update: request method and client id

--- src/pages/api/ably-token.ts
+++ src/pages/api/ably-token.ts
@@ -5,23 +5,19 @@ import { NextApiHandler } from "next"
 
 const handler: NextApiHandler = async (req, res) => {
 	try {
-		if (req.method !== "POST") {
+		if (req.method !== "GET") {
 			throw new CustomError("Method not allowed", {
 				statusCode: 405,
 			})
 		}
 
-		const { id } = req.body
-
-		if (!id) throw new CustomError("Missing parameters")
-
 		const client = new Realtime(process.env.ABLY_API_KEY!)
 
 		const ablyTokenRequest = await client.auth.createTokenRequest({
-			clientId: id,
+			clientId: "findzy-chat",
 		})
 
-		return res.status(200).json({ ablyTokenRequest })
+		return res.status(200).json(ablyTokenRequest)
 	} catch (e) {
 		const { error, statusCode } = handleError(e)
 		return res.status(statusCode).json({ error })
