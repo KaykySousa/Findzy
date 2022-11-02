6d7b8e3c937c9d4547dcf5d96c426b8a1d0f74b5
KaykySousa <kaykysousa321@gmail.com>
2022-11-02 14:48:59 -0300

    add: api route to set claim message

---
+++ src/pages/api/send-claim-message.ts
@@ -0,0 +1,93 @@
+import prisma from "@/prisma/client"
+import CustomError from "@/utils/CustomError"
+import getCompany from "@/utils/getCompany"
+import getUser from "@/utils/getUser"
+import handleError from "@/utils/handleError"
+import validateToken from "@/utils/validateToken"
+import { NextApiHandler } from "next"
+
+const handler: NextApiHandler = async (req, res) => {
+	try {
+		if (req.method !== "POST") {
+			throw new CustomError("Method not allowed", {
+				statusCode: 405,
+			})
+		}
+
+		let fromId = ""
+
+		const token = req.headers.authorization?.replace("Bearer ", "")
+
+		const decodedToken = validateToken(token)
+
+		if (!decodedToken) {
+			throw new CustomError("Token not provided")
+		}
+
+		if (decodedToken.accountType === "user") {
+			const user = await getUser(decodedToken.sub!)
+
+			if (!user) {
+				throw new CustomError("Unauthorized", {
+					statusCode: 401,
+				})
+			}
+
+			fromId = user.id
+		}
+
+		if (decodedToken.accountType === "company") {
+			const company = await getCompany(decodedToken.sub!)
+
+			if (!company) {
+				throw new CustomError("Unauthorized", {
+					statusCode: 401,
+				})
+			}
+
+			fromId = company.id
+		}
+
+		const { toId, content } = req.body
+
+		const message = await prisma.message.create({
+			data: {
+				content,
+				sender_id: fromId,
+				conversation: {
+					connectOrCreate: {
+						where: {
+							user_id_company_id: {
+								user_id:
+									decodedToken.accountType === "user"
+										? fromId
+										: toId,
+								company_id:
+									decodedToken.accountType === "company"
+										? fromId
+										: toId,
+							},
+						},
+						create: {
+							company_id:
+								decodedToken.accountType === "company"
+									? fromId
+									: toId,
+							user_id:
+								decodedToken.accountType === "user"
+									? fromId
+									: toId,
+						},
+					},
+				},
+			},
+		})
+
+		return res.status(201).json({ message })
+	} catch (e) {
+		const { error, statusCode } = handleError(e)
+		return res.status(statusCode).json({ error })
+	}
+}
+
+export default handler
