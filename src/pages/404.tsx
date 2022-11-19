ccf6ae412e3ddad408406ec850733f62ccf8469d
KaykySousa <kaykysousa321@gmail.com>
2022-11-19 18:06:26 -0300

    add: SEO

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "@/components/index"
+import SEO from "@/components/SEO"
 import { LinkButton } from "@/design/index"
 
 export default function Error404() {
 	return (
 		<div className="flex min-h-screen w-full flex-col items-center p-10">
+			<SEO title="404" description="Página não encontrada" />
 			<Logo className="!text-xl" />
 			<div className="flex w-full max-w-lg flex-1 flex-col items-center justify-center gap-y-6">
 				<h1 className="text-6xl font-bold text-purple-700">404</h1>
