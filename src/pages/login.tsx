0179a86487f70f2b296fb5add88255ad573b0d83
KaykySousa <kaykysousa321@gmail.com>
2022-11-22 10:08:38 -0300

    add: link company register

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -57,18 +57,26 @@ export default function Login() {
 				description="Ingresse no Findzy e encontre facilmente seu objeto desaparecido"
 			/>
 			<div className="hidden w-2/5 flex-col bg-purple-700 p-10 lg:flex">
-				<Logo className="hiden mb-4 !text-xl !text-white lg:block" />
-				<div className="flex flex-1 flex-col items-center justify-center gap-y-8">
-					<p className="text-center text-3xl font-bold text-white">
-						Primeira vez?
-					</p>
-					<p className="text-center text-lg text-white">
-						Conheça a nossa plataforma de achados e perdidos
-					</p>
-					<LinkButton href="/register" theme="secondary">
-						Cadastre-se
-					</LinkButton>
+				<div className="flex flex-col justify-center w-full h-full">
+					<Logo className="hiden mb-4 !text-xl !text-white lg:block" />
+					<div className="flex flex-1 flex-col items-center justify-center gap-y-8">
+						<p className="text-center text-3xl font-bold text-white">
+							Primeira vez?
+						</p>
+						<p className="text-center text-lg text-white">
+							Conheça a nossa plataforma de achados e perdidos
+						</p>
+						<LinkButton href="/register/user" theme="secondary">
+							Cadastre-se
+						</LinkButton>
+					</div>
 				</div>
+				<p className="mt-4 text-purple-200 text-sm font-semibold text-center">
+					É responsável por um estabelecimento?&nbsp;
+					<Anchor href="/register/company" className="text-white">
+					Cadastre-o agora.
+				</Anchor>
+				</p>
 			</div>
 			<div className="flex flex-1 flex-col items-center p-5 md:justify-center lg:p-10">
 				<div className="flex w-full max-w-xl flex-1 flex-col items-center md:justify-center">
@@ -84,7 +92,7 @@ export default function Login() {
 					</p>
 					<form
 						id="login-form"
-						className="mb-12 flex w-full flex-col gap-y-6"
+						className="mb-10 flex w-full flex-col gap-y-6"
 						onSubmit={handleSubmit}
 					>
 						<FloatingInput
@@ -96,18 +104,20 @@ export default function Login() {
 								setEmail(e.target.value)
 							}}
 						/>
-						<FloatingInput
-							placeholder="Senha"
-							togglePassword
-							required
-							value={password}
-							onChange={(e) => {
-								setPassword(e.target.value)
-							}}
-						/>
-						<Anchor href="/forgot-password">
-							Esqueci minha senha
-						</Anchor>
+						<div className="flex flex-col w-full">
+							<FloatingInput
+								placeholder="Senha"
+								togglePassword
+								required
+								value={password}
+								onChange={(e) => {
+									setPassword(e.target.value)
+								}}
+							/>
+							{/* <Anchor href="/forgot-password" theme="second" className="mt-3">
+								Esqueci minha senha
+							</Anchor> */}
+						</div>
 					</form>
 					<Button
 						type="submit"
