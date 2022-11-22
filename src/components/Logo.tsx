79a07e2f44b2fb0bb66d1805fb24ad993bd2e1af
KaykySousa <kaykysousa321@gmail.com>
2022-11-22 10:07:03 -0300

    update: font weight

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,8 +7,8 @@ export default function Logo({ className, ...props }: LogoProps) {
 	return (
 		<Link href="/">
 			<a className={`text-lg text-purple-700 ${className}`}>
-				<p className="whitespace-nowrap" {...props}>
-					<strong>Findzy</strong> Perdeu? Achou!
+				<p className="whitespace-nowrap font-light" {...props}>
+					<span className="font-extrabold">Findzy</span> Perdeu? Achou!
 				</p>
 			</a>
 		</Link>
