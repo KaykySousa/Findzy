8a48d0efa38c47c44c4bf88bb5b4b9fbcc0a5161
KaykySousa <kaykysousa321@gmail.com>
2022-11-23 23:15:54 -0300

    add: api route to update items data

---
+++ src/pages/api/update-item.ts
@@ -0,0 +1,110 @@
+import prisma from "@/prisma/client"
+import CustomError from "@/utils/CustomError"
+import handleError from "@/utils/handleError"
+import imageUploader from "@/utils/imageUploader"
+import { NextApiHandler, PageConfig } from "next"
+
+interface ImagesUrlData {
+	image_url: string
+}
+
+interface BodyData {
+	category: string
+	color: string
+	description: string
+	local: string
+	name: string
+	companyId: string
+	images: string[]
+	itemId: string
+}
+
+const handler: NextApiHandler = async (req, res) => {
+	try {
+		if (req.method !== "PUT") {
+			throw new CustomError("Method not allowed", {
+				statusCode: 405,
+			})
+		}
+
+		let imagesUrl: ImagesUrlData[] = []
+
+		const {
+			category,
+			color,
+			description,
+			local,
+			name,
+			images,
+			companyId,
+			itemId,
+		}: BodyData = req.body
+
+		if (
+			!category ||
+			!color ||
+			!description ||
+			!local ||
+			!name ||
+			!images ||
+			!companyId ||
+			!itemId
+		) {
+			throw new CustomError("Missing parameters")
+		}
+
+		await new Promise((resolve, reject) => {
+			images.map(async (image) => {
+				const { secure_url: imageUrl } = await imageUploader(
+					image,
+					"findzy-items"
+				)
+				console.log(imageUrl)
+				imagesUrl.push({ image_url: imageUrl })
+				if (imagesUrl.length === images.length) {
+					resolve(imagesUrl)
+				}
+			})
+		})
+
+		await prisma.itemImage.deleteMany({
+			where: {
+				item_id: itemId,
+			},
+		})
+
+		const item = await prisma.item.update({
+			where: {
+				id: itemId,
+			},
+			data: {
+				category,
+				color,
+				description,
+				local,
+				name,
+				images: {
+					createMany: {
+						data: imagesUrl,
+					},
+				},
+				company_id: companyId,
+			},
+		})
+
+		return res.status(201).json(item)
+	} catch (e) {
+		const { error, statusCode } = handleError(e)
+		return res.status(statusCode).json({ error })
+	}
+}
+
+export const config: PageConfig = {
+	api: {
+		bodyParser: {
+			sizeLimit: "16mb",
+		},
+	},
+}
+
+export default handler
