1db2236ea55582e5ac2377ad5b3b7e262deec42f
KaykySousa <kaykysousa321@gmail.com>
2022-11-23 23:07:24 -0300

    add: icon button component

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,12 +2,16 @@ import { ButtonHTMLAttributes } from "react"
 
 interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export default function IconButton(props: IconButtonProps) {
-    return (
-        <button
-            type="button"
-            className="absolute right-0 rounded-full p-1 text-gray-500 outline-purple-700 transition-colors hover:bg-purple-200 peer-focus:text-purple-700"
-            {...props}
-        />
-    )
+export default function IconButton({
+	className,
+	type,
+	...props
+}: IconButtonProps) {
+	return (
+		<button
+			type={type || "button"}
+			className={`flex-shrink-0 rounded-full p-1 text-gray-500 outline-purple-700 transition-colors hover:bg-purple-200 peer-focus:text-purple-700 ${className}`}
+			{...props}
+		/>
+	)
 }
