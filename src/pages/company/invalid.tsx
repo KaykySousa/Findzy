4c41bb587130514cbce67cca679c6d73f131624d
KaykySousa <kaykysousa321@gmail.com>
2022-12-04 20:29:09 -0300

    add: invalid page

---
+++ src/pages/company/invalid.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/design"
+import { Logo } from "@/components/index"
+import SEO from "@/components/SEO"
+import getCompanyById from "@/utils/getCompanyById"
+import logout from "@/utils/logout"
+import validateToken from "@/utils/validateToken"
+import { GetServerSideProps } from "next"
+import { parseCookies } from "nookies"
+
+export default function Review() {
+	return (
+		<div className="flex min-h-screen w-full flex-col items-center p-10">
+			<SEO
+				title="Conta negada"
+				description="A empresa não passou no processo de análise. Entre em contato para mais informações."
+			/>
+			<Logo className="!text-xl" />
+			<div className="flex w-full max-w-lg flex-1 flex-col items-center justify-center gap-y-6">
+				<h1 className="text-6xl font-bold text-purple-700">
+					ACESSO NEGADO
+				</h1>
+				<p className="text-center text-3xl">
+					A conta não passou no processo de análise. Para mais
+					informações entre em contato:{" "}
+					<span className="text-purple-700">
+						contato.findzy@gmail.com
+					</span>
+				</p>
+				<Button
+					onClick={() => {
+						logout()
+					}}
+				>
+					Sair
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+	const { "findzy.token": token } = parseCookies(ctx)
+
+	const decodedToken = validateToken(token)
+
+	if (!decodedToken) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/login",
+			},
+		}
+	}
+
+	const company = await getCompanyById(decodedToken.sub!)
+
+	if (!company) {
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/",
+			},
+		}
+	}
+
+	if (company.status !== "invalid")
+		return {
+			redirect: {
+				permanent: false,
+				destination: "/",
+			},
+		}
+
+	return {
+		props: {
+			a: "b",
+		},
+	}
+}
