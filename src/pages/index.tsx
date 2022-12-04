341ad68b601fe034732d497c80d30e1790685aaa
KaykySousa <kaykysousa321@gmail.com>
2022-12-04 19:44:21 -0300

    update: move /pages/user/index to /pages/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,79 @@
-import getAdmin from "@/utils/getAdmin"
-import getCompany from "@/utils/getCompany"
-import getUser from "@/utils/getUser"
-import validateToken from "@/utils/validateToken"
-import { GetServerSideProps } from "next"
-import { parseCookies } from "nookies"
+import CompanyCard from "@/components/CompanyCard"
+import { Header } from "@/components/index"
+import SEO from "@/components/SEO"
+import prisma from "@/prisma/client"
+import withAuth from "@/utils/withAuth"
+import { Address } from "@prisma/client"
+import { useRouter } from "next/router"
 
-export default function Index() {
-	return <div></div>
+interface MainUserProps {
+	companies: {
+		address: Address
+		id: string
+		name: string
+		profile_picture_url: string
+		_count: {
+			items: number
+			companies_reports: number
+		}
+	}[]
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const { "findzy.token": token } = parseCookies(ctx)
+export default function MainUser({ companies }: MainUserProps) {
+	const router = useRouter()
 
-	const decodedToken = validateToken(token)
+	return (
+		<div className="min-h-screen w-full bg-white">
+			<SEO title="Início" description="Página inicial." />
+			<Header />
+			<div className="flex w-full flex-col items-center gap-y-2 p-2 md:p-0 md:py-4">
+				{companies.map((company, index) => (
+					<CompanyCard
+						key={index}
+						company={company.name}
+						address={`${company.address.street}, ${company.address.number} - ${company.address.district} - ${company.address.uf}`}
+						logoUrl={company.profile_picture_url}
+						itemsNumber={company._count.items}
+						rating={Math.max(
+							5 - company._count.companies_reports / 10,
+							0
+						)}
+						href={`/company/${company.id}`}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
 
-	if (!decodedToken) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/login",
+export const getServerSideProps = withAuth(["user"], async ({ data }) => {
+	const companies = await prisma.company.findMany({
+		select: {
+			id: true,
+			name: true,
+			address: {
+				select: {
+					cep: true,
+					city: true,
+					district: true,
+					number: true,
+					street: true,
+					uf: true,
+				},
 			},
-		}
-	}
-
-	const userData = await getUser(decodedToken.sub!)
-
-	if (userData)
-		return {
-			redirect: {
-				permanent: false,
-				destination: "/user",
+			profile_picture_url: true,
+			_count: {
+				select: {
+					items: true,
+					companies_reports: true,
+				},
 			},
-		}
-
-	const companyData = await getCompany(decodedToken.sub!)
-
-	if (companyData)
-		return {
-			redirect: {
-				permanent: false,
-				destination: `/company/${companyData.id}`,
-			},
-		}
-
-	const adminData = await getAdmin(decodedToken.sub!)
-
-	if (adminData)
-		return {
-			redirect: {
-				permanent: false,
-				destination: `/admin`,
-			},
-		}
+		},
+	})
 
 	return {
-		redirect: {
-			permanent: false,
-			destination: "/login",
+		props: {
+			companies,
 		},
 	}
-}
+})
